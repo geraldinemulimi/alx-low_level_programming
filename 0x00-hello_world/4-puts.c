8c5d7ae2010ab1959bf314d0e25064dabb703559
geraldinemulimi <geraldinemulimi@gmail.com>
2022-03-10 10:54:48 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
-
-
+#include<stdio.h>
 
 /**
 
- * main - prints the phrase "Programming is like building a multilingual puzzle
+    * main - Entry point
 
- * Return: 0 if exited properly, non-zero otherwise
+        *
 
- */
+             * Return: Always 0 (Success)
+
+                    */
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+			puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+						return (0);
 
 }
