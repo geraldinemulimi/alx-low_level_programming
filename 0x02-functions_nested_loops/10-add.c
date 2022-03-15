64eea0a993ac0486cbd4ad7a2b4d9c03e49e3112
geraldinemulimi <geraldinemulimi@gmail.com>
2022-03-15 15:57:51 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+/*
+ * File: 10-add.c
+ * Auth: Samuel Kioko
+ */
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Result of addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
