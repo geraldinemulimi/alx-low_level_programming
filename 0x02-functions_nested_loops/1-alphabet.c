e5687bc042766fd606adc5fc3d2bd5984f40f723
geraldinemulimi <geraldinemulimi@gmail.com>
2022-03-15 13:42:37 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: Geraldine Mulimi
+ *
+ */
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ *
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+	_putchar('\n');
+}
