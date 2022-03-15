2a5c61ec5766a9798cf0597bda4db18f06e2464f
geraldinemulimi <geraldinemulimi@gmail.com>
2022-03-15 13:54:50 -0700

    filetwo

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: Geraldine Mulimi
+ *
+ */
+#include "main.h"
+/**
+ * print_alphabet_x10 -  prints 10 times the alphabet,
+ * in lowercase, followed by a new line.
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int counter;
+	char x;
+
+	for (counter = 0; counter <= 9; counter++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+
+}
