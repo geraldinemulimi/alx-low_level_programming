114f51443908a069018364c3528c2c7f486725f8
geraldinemulimi <geraldinemulimi@gmail.com>
2022-04-11 18:50:19 +0000

    frees dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -10,10 +10,10 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d)
-	{
-		free(d->name);
-		freefree(d->owner);
-		free(d);
-	}
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	freefree(d->owner);
+	free(d);
 }
