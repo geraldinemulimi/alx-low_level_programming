5e1f3b77a353377341e6eaa00010fb5c99f153f9
geraldinemulimi <geraldinemulimi@gmail.com>
2022-06-11 02:59:15 +0000

    doubly linked lists

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,7 +10,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *tmp;
 	unsigned int c;
-	
+
 	tmp = head;
 	for (c = 0; tmp && c < index; tmp = tmp->next, c++)
 		;
